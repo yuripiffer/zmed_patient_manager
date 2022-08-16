4b1b3d96f7d2b884174b03b51d98b6743765f440
yuripiffer <yuripiffer@hotmail.com>
2022-08-16 20:10:51 -0300

    phone

---
+++ domain/patient/phone.go
@@ -0,0 +1,37 @@
+package patient
+
+import (
+	"strconv"
+	"zmed_patient_manager/pkg/app_errors"
+)
+
+func (s *service) validatePhone(cellphone string) bool {
+	_, err := strconv.Atoi(cellphone)
+	if err != nil {
+		return false
+	}
+	if len(cellphone) >= 11 {
+		if cellphone[len(cellphone)-9:len(cellphone)-8] != "9" {
+			return false
+		}
+		return true
+
+	}
+	return false
+}
+
+func (s *service) encryptPhone(cellphone string) (string, app_errors.AppError) {
+	cellphoneCipher, appErr := s.crypto.Encrypt(cellphone)
+	if appErr != nil {
+		return "", appErr
+	}
+	return cellphoneCipher, nil
+}
+
+func (s *service) decryptPhone(cipher string) (string, app_errors.AppError) {
+	cellphone, appErr := s.crypto.Decrypt(cipher)
+	if appErr != nil {
+		return "", appErr
+	}
+	return cellphone, nil
+}

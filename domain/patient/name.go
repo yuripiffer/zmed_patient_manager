f9b2e58fe5ac895c884d0e0b1849b7f67bb384bb
yuripiffer <yuripiffer@hotmail.com>
2022-08-16 21:07:50 -0300

    updates in email, name and phone validation functions

--- domain/patient/name.go
+++ domain/patient/name.go
@@ -1,1 +1,37 @@
 package patient
+
+import (
+	"strings"
+	"zmed_patient_manager/pkg/app_errors"
+)
+
+func (s *service) validateName(name string) bool {
+	splitName := strings.Split(name, " ")
+	if !(len(splitName) >= 2) {
+		return false
+	}
+	if strings.Contains(name, "!") ||
+		strings.Contains(name, "@") ||
+		strings.Contains(name, "$") ||
+		strings.Contains(name, "%") ||
+		strings.Contains(name, "*") {
+		return false
+	}
+	return true
+}
+
+func (s *service) encryptName(name string) (string, app_errors.AppError) {
+	nameCipher, appErr := s.crypto.Encrypt(name)
+	if appErr != nil {
+		return "", appErr
+	}
+	return nameCipher, nil
+}
+
+func (s *service) decryptName(cipher string) (string, app_errors.AppError) {
+	name, appErr := s.crypto.Decrypt(cipher)
+	if appErr != nil {
+		return "", appErr
+	}
+	return name, nil
+}
